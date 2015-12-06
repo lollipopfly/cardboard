c4510470a78603c6a2c378dfb42764baadf01b61
lollipop.fly@gmail.com <lollipop.fly@gmail.com>
2015-12-06 21:43:42 +0300

    make gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp         = require('gulp'),
 	postcss      = require('gulp-postcss'),
 	sass         = require('gulp-sass'),
 	autoprefixer = require('autoprefixer'),
+	concat       = require('gulp-concat'),
 	browserSync  = require('browser-sync').create(),
 	selectors    = require('postcss-custom-selectors'),
 	spritesmith  = require('gulp.spritesmith'),
 	plumber      = require('gulp-plumber'),
+	notify       = require("gulp-notify"),
 	merge        = require('merge-stream');
 
 /*------------------------------------*\
@@ -16,12 +18,16 @@ gulp.task('sass', function() {
 	var processors = [
 		autoprefixer({ browsers: ['last 20 versions'] }),
 		require('postcss-font-magician')({}),
-		 selectors,
+		selectors,
 	];
 
-	return gulp.src('sass/style.scss')
+	return gulp.src(['sass/reset.scss',
+					'sass/main.scss',
+					'css/sprite.css',
+					'sass/style.scss',])
 		.pipe(plumber())
 		//.pipe(sass().on('error', error))
+		.pipe(concat('style.css'))
 		.pipe(sass(
 			// {outputStyle: 'compressed'}
 			))
@@ -38,8 +44,16 @@ gulp.task('sprite', function () {
 	var spriteData = gulp.src('images/main/*.png').pipe(spritesmith({
 		imgName: 'sprite.png',
 		cssName: 'sprite.css',
+		imgPath: '../images/sprite.png',
 		padding: 1,
-	}));
+		cssOpts: {
+		// for remove prefix icon-
+		cssSelector: function (sprite) {
+			return '.' + sprite.name;
+		},
+		algorithmOpts: {sort: true}
+	}
+}));
 
 	var imgStream = spriteData.img
 		.pipe(gulp.dest('images/'));
@@ -70,8 +84,19 @@ gulp.task('serve', ['sass'], function() {
 \*------------------------------------*/
 
 gulp.task('watch', function() {
-	gulp.watch('sass/**/*.scss', { interval: 500 }, ['sass']);
+	gulp.watch('sass/**/*.scss', { interval: 500 }, ['sass', 'notify']);
 	// gulp.watch('images/main/*.png', { interval: 500 }, ['sprite']);
+});
+
+
+/*------------------------------------*\
+    Notify
+\*------------------------------------*/
+
+gulp.task('notify', function() {
+    var date = new Date();
+    gulp.src("css/style.css")
+    .pipe(notify("Css was compiled! at " + date));
 });
 
 /*------------------------------------*\

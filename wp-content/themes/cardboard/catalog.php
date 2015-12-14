87c55b8fd6be45c9da92e7ccd87a031bea2bf768
lollipop.fly@gmail.com <lollipop.fly@gmail.com>
2015-12-14 12:43:45 +0300

    complete main cat page

--- wp-content/themes/cardboard/catalog.php
+++ wp-content/themes/cardboard/catalog.php
@@ -10,27 +10,26 @@ Template Name: Сatalog
 	$wp_query = new WP_Query();
 	$wp_query->query('showposts=15' . '&paged='.$paged);
 	if($wp_query):?>
-		<ul class="recent row">
+		<ul class="recent recent--cat row">
 			<? while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-				<li class="recent__item col-md-6 col-sm-12 col-xs-12">
+				<li class="recent__item">
 					<? $cost = get_post_meta(get_the_ID(), 'cost');
 					   $old_cost = get_post_meta(get_the_ID(), 'old-cost');
 					   $info = get_post_meta(get_the_ID(), 'preview');
 					?>
-					<!-- Выводим адаптивную миниатюру иначе будет с фиксированными размерами -->
 					<?	if ( has_post_thumbnail() ) {
-					    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),'thumbnail' );
-					     echo '<img width="100%" src="' . $image_src[0] . '">';
-					}?>
+					    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),'thumbnail' );?>
+					     <a href="<? the_permalink()?>"><img width="100%" src="<?=$image_src[0]?>"></a>
+					<?}?>
 					<a class="recent__name" href="<?php the_permalink(); ?>"><? the_title(); ?></a>
 					<? if($info[0]):?>
 						<span class="recent__info"><?=$info[0];?></span>
 					<?endif;?>
 					<? if($cost[0]):?>
-						<p class="recent__price"><?=$cost[0];?></p>
+						<p class="recent__price"><?=$cost[0];?> руб.</p>
 					<?endif;?>
 					<? if($old_cost[0]):?>
-						<p class="recent__discount"><?=$old_cost[0];?></p>
+						<p class="recent__discount"><?=$old_cost[0];?> руб.</p>
 					<?endif;?>
 					<a href="#" class="recent__order btn btn-order">Заказать</a>
 				</li>

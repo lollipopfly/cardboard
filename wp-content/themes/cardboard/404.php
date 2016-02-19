18292b6c5db9c6671afbea0e7a17cdff4492058e
lollipop.fly@gmail.com <lollipop.fly@gmail.com>
2016-02-19 21:45:24 +0300

    make 404 page

---
+++ wp-content/themes/cardboard/404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-lg-7 col-sm-10 col-xs-12">
+			<h1>404 PAGE</h1>
+			<h4>Oops.. somethign goin wrong</h4>
+			<div class="img-container text-center">
+				<img src="<? bloginfo('template_url'); ?>/images/tyson-smiling.gif" alt="404 page" style="width:100%">
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
+
